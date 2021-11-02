383cb4be3d5766e639c872cb76c737ff03038eda
yearsky <yearskylifael@Yearskys-MacBook-Pro.local>
2021-11-02 09:44:29 +0800

    new added quiz

--- database/migrations/2021_10_06_040932_create_procedure_absensi.php
+++ database/migrations/2021_10_06_040932_create_procedure_absensi.php
@@ -15,7 +15,7 @@ class CreateProcedureAbsensi extends Migration
     {
         $procedure = "DROP PROCEDURE IF EXISTS `Absensi`;
         CREATE DEFINER=`".env('DB_USERNAME')."`@`".env('DB_HOST')."` PROCEDURE `Absensi`()
-            BEGIN SELECT Absensi.id, Concat(instructors.first_name, ' ', instructors.last_name) AS Guru, Concat(Users.first_name,' ',Users.last_name) As Siswa, course_title,keterangan FROM Absensi INNER JOIN Instructors ON Absensi.id_instructor = Instructors.id  INNER JOIN Users ON Absensi.id_siswa = Users.id INNER JOIN Courses ON Absensi.id_course = Courses.id; 
+            BEGIN SELECT Absensi.id, Concat(instructors.first_name, ' ', instructors.last_name) AS Guru, Concat(Users.first_name,' ',Users.last_name) As Siswa, table_kelas.nama as Kelas ,course_title as Mapel,keterangan FROM Absensi INNER JOIN Instructors ON Absensi.id_instructor = Instructors.id  INNER JOIN Users ON Absensi.id_siswa = Users.id INNER JOIN Courses ON Absensi.id_course = Courses.id INNER JOIN table_kelas ON Absensi.id_kelas = table_kelas.id; 
         END;";
 
         DB::unprepared($procedure);
