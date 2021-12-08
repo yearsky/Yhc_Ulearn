04f7673e1f28fef1375537f9ee9391dc59a6731b
Bayu Gusti Paraya <bayu.gustip7820@gmail.com>
2021-12-08 13:47:04 +0800

    coursetaken migration

--- database/migrations/2019_02_22_151526_create_course_taken_table.php
+++ database/migrations/2019_02_22_151526_create_course_taken_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePaymentsTable extends Migration
+class CreateCourseTakenTable extends Migration
 {
     /**
      * Run the migrations.
